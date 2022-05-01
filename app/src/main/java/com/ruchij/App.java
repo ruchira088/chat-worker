22b20a64c736d15e75d74ed2411ab1a24381a362
Ruchira (Richie) Jayasekara <me@ruchij.com>
2022-05-01 23:45:07 +1000

    Adding typesafe config

--- app/src/main/java/com/ruchij/App.java
+++ app/src/main/java/com/ruchij/App.java
@@ -1,11 +1,14 @@
 package com.ruchij;
 
+import com.ruchij.config.KafkaConfiguration;
+import com.typesafe.config.ConfigFactory;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println(KafkaConfiguration.fromConfig(ConfigFactory.defaultApplication()));
     }
 }
--- app/src/main/java/com/ruchij/config/KafkaConfiguration.java
+++ app/src/main/java/com/ruchij/config/KafkaConfiguration.java
@@ -1,4 +1,16 @@
 package com.ruchij.config;
 
+import com.typesafe.config.Config;
+
 public record KafkaConfiguration(String bootstrapServers, String schemaRegistryUrl) {
+
+    public static KafkaConfiguration fromConfig(Config config) {
+        Config kafkaConfig = config.getConfig("kafka-configuration");
+
+        String bootstrapServers = kafkaConfig.getString("bootstrap-servers");
+        String schemaRegistryUrl = kafkaConfig.getString("schema-registry-url");
+
+        return new KafkaConfiguration(bootstrapServers, schemaRegistryUrl);
+    }
+
 }
